b17a8e13eaa0e7ed7b233283eae62ba53df1a082
Ekaterina Elesova <elesova.e@gmail.com>
2017-12-22 02:50:09 +0300

    удален лишний код

--- js/helpers.js
+++ js/helpers.js
@@ -3,41 +3,6 @@
 (function () {
   window.helpers = {
 
-    copyArray: function (array, newArray) {
-      for (var i = 0; i < array.length; i++) {
-        newArray.push(array[i]);
-      }
-    },
-
-    getRandomIndex: function (array) {
-      return Math.floor(Math.random() * array.length);
-    },
-
-    getUniqueRandomValue: function (array) {
-      var index = window.helpers.getRandomIndex(array);
-      var value = array[index];
-
-      array.splice(index, 1);
-      return value;
-    },
-
-    getRandomNumber: function (startNumber, endNumber) {
-      return Math.floor(Math.random() * endNumber) + startNumber;
-    },
-
-    getRandomArray: function (array) {
-      var randomArray = [];
-      window.helpers.copyArray(array, randomArray);
-
-      for (var j = randomArray.length - 1; j > 0; j--) {
-        var index = window.helpers.getRandomIndex(randomArray);
-        var value = randomArray[index];
-        randomArray[index] = randomArray[j];
-        randomArray[j] = value;
-      }
-      return randomArray.slice(window.helpers.getRandomIndex(randomArray));
-    },
-
     hideElement: function (elem) {
       elem.classList.add('hidden');
     },
